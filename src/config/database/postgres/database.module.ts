33659f678e5dbf44374fa2216ae2a428ddf3683f
adityahazarika <adityahazarika08@gmail.com>
2022-08-30 17:04:00 +0530

    some db modifications

--- src/config/database/postgres/database.module.ts
+++ src/config/database/postgres/database.module.ts
@@ -4,13 +4,27 @@ import { Pool } from 'pg';
 import { DatabaseService } from './database.service';
 
 const databasePoolFactory = async (configService: ConfigService) => {
-    return new Pool({
+    const pool = new Pool({
       user: configService.get('DB_USER'),
       host: configService.get('DB_HOST'),
       database: configService.get('DB_DATABASE'),
       password: configService.get('DB_PASSWORD'),
       port: configService.get('DB_PORT'),
+      idleTimeoutMillis: 30000
     });
+    pool.on('connect', client => {
+        console.log("new client connected")
+    })
+    pool.query('SELECT NOW()', (err, cl) => {
+        console.log(cl.rows)
+    })
+    pool.on('error', (err, client) => {
+        console.log("Database error")
+    })
+    pool.on('remove', (client) => {
+        console.log("Database connection removed")
+    })
+    return pool
   };
 
 @Module({
